548bfd6d1adbf2f0ab600c4246451b8108138b2e
Grayson Cunha <graysonsilva93@gmail.com>
2022-10-19 11:29:46 -0300

    feat(user): implement create, read, update and delete

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.userRepository.create({ email, password });
+
+    return this.userRepository.save(user);
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  find(email: string) {
+    return this.userRepository.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    const newUser: User = { ...user, ...attrs };
+    return this.userRepository.save(newUser);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return this.userRepository.remove(user);
+  }
+}
