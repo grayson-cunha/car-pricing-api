e64805478716db5f8df3288fb54dbba0312f34a6
Grayson Cunha <graysonsilva93@gmail.com>
2022-11-08 15:30:03 -0300

    feat(reports): include endpoint to create report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report-dto';
+import { Report } from './reports.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private reportsRepository: Repository<Report>,
+  ) {}
+
+  create(reportDto: CreateReportDto): Promise<Report> {
+    return this.reportsRepository.save(reportDto);
+  }
+}
