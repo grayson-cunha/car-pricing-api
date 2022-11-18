d2bcf7c31159c0d4cb8ecccdd6adf51c52dee777
Grayson Cunha <graysonsilva93@gmail.com>
2022-11-18 10:57:04 -0300

    feat(report): allowed to approve report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { SerializeResponseTo } from '../interceptors/serialize.interceptor';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
@@ -6,6 +13,7 @@ import { User } from '../users/user.entity';
 import { CreateReportDto } from './dtos/create-report-dto';
 import { ReportsService } from './reports.service';
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -15,10 +23,14 @@ export class ReportsController {
   @UseGuards(AuthGuard)
   @SerializeResponseTo(ReportDto)
   async createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
-    const test = await this.reportsService.create(body, user);
+    return await this.reportsService.create(body, user);
+  }
 
-    console.log(test);
-
-    return test;
+  @Patch('/:id')
+  approveReport(
+    @Param('id') id: string,
+    @Body() { approved }: ApproveReportDto,
+  ) {
+    return this.reportsService.changeApproval(id, approved);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { Repository } from 'typeorm';
@@ -15,6 +15,20 @@ export class ReportsService {
     const report: Report = this.reportsRepository.create(reportDto);
 
     report.user = user;
+
+    return this.reportsRepository.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.reportsRepository.findOne({
+      where: { id: parseInt(id) },
+    });
+
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
 
     return this.reportsRepository.save(report);
   }
