cd7fa81bbdadc4151d92a203163365e36f8ac90a
Grayson Cunha <graysonsilva93@gmail.com>
2022-11-28 10:57:02 -0300

    feat(reports): allowed to get estimate

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { User } from '../users/user.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report-dto';
 import { Report } from './reports.entity';
+import { GetEstimateDto } from './dtos/get-estimate-dto';
 
 @Injectable()
 export class ReportsService {
@@ -31,5 +32,19 @@ export class ReportsService {
     report.approved = approved;
 
     return this.reportsRepository.save(report);
+  }
+
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.reportsRepository
+      .createQueryBuilder()
+      .select('*')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .orderBy('mileage - :mileage', 'DESC')
+      .setParameters({ mileage })
+      .getRawMany();
   }
 }
